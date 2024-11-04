e501c0da6956a7b642f3acf65ff5ea9030d58223
Joarell <josephjoarell@gmail.com>
2024-11-04 10:51:00 -0300

    feat (update): adds new plugins, settings and zellij layout

--- nvim/lua/plugins/init.lua
+++ nvim/lua/plugins/init.lua
@@ -458,6 +458,21 @@ return {
 	--  ╰──────────────────────────────────────────────────────────╯
 	{ "tpope/vim-fugitive" },
 	{ "tveskag/nvim-blame-line" },
+	{ 
+		"f-person/git-blame.nvim",
+		event = "VeryLazy",
+		config = function ()
+			require("gitblame").setup({})
+		end,
+		opts = {
+			-- your configuration comes here
+			-- for example
+			enabled = true,  -- if you want to enable the plugin
+			message_template = " <summary> • <date> • <author> • <<sha>>", -- template for the blame message, check the Message template section for more options
+			date_format = "%m-%d-%Y %H:%M:%S", -- template for the date, check Date format section for more options
+			virtual_text_column = 1,  -- virtual text start column, check Start virtual text at column section for more options
+		},
+	},
 	-- {
 	-- 	"tanvirtin/vgit.nvim",
 	-- 	dependencies = {
