cedaa242ce2c44d855ca26eb860a605713dd0d61
jev <josephjoarell@gmail.com>
2025-06-29 00:52:59 -0300

    core(update): remove the rustowl plugin

--- nvim/lua/plugins/rustowl.lua
+++ nvim/lua/plugins/rustowl.lua
@@ -1,17 +1,17 @@
 return {
-	{
-		"cordx56/rustowl",
-		version = "*", -- Latest stable version
-		build = "cargo binstall rustowl",
-		lazy = false, -- This plugin is already lazy
-		opts = {
-			client = {
-				on_attach = function(_, buffer)
-					vim.keymap.set("n", "<leader>o", function()
-						require("rustowl").toggle(buffer)
-					end, { buffer = buffer, desc = "Toggle RustOwl" })
-				end,
-			},
-		},
-	},
+	-- {
+	-- 	"cordx56/rustowl",
+	-- 	version = "*", -- Latest stable version
+	-- 	build = "cargo binstall rustowl",
+	-- 	lazy = false, -- This plugin is already lazy
+	-- 	opts = {
+	-- 		client = {
+	-- 			on_attach = function(_, buffer)
+	-- 				vim.keymap.set("n", "<leader>o", function()
+	-- 					require("rustowl").toggle(buffer)
+	-- 				end, { buffer = buffer, desc = "Toggle RustOwl" })
+	-- 			end,
+	-- 		},
+	-- 	},
+	-- },
 }
